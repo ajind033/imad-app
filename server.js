491935c97f66baecac1583f2e992296ba77f42bf
Akash Jindal <30688034+ajind033@users.noreply.github.com>
2017-08-09 15:21:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res) {
+    res.sendFile("Article One has been requested and has been served.");
+});
+app.get('/article-two',function(req,res) {
+    res.sendFile("Article Two has been requested and has been served.");
+});
+app.get('/article-three',function(req,res) {
+    res.sendFile("Article Three has been requested and has been served.");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
