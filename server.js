79289001ea87edd2eb583504f80aac648ae12c1d
Akash Jindal <30688034+ajind033@users.noreply.github.com>
2017-08-12 16:03:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
 
 app.get('/articleName',function(req,res) {
   var  articleName1 = req.params.articeName;
-  res.send(createTemp(createTemp(articleName1)));
+  res.send(createTemp(articles[articleName1]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
