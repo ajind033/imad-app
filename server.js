bed17293aed751062a933636639782860faef426
Akash Jindal <30688034+ajind033@users.noreply.github.com>
2017-08-12 15:47:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var article-one =
+{
+    title: 'Article-One|Bitcoin',
+    date: '12/August/2017',
+    heading: 'Article-One|Bitcoin',
+    content:  '
+            <p>
+                Bitcoin is a worldwide cryptocurrency and digital payment system[13]:3 invented by an unknown programmer, or a group of programmers, under the name Satoshi Nakamoto.[14] It was released as open-source software in 2009.[15]
+
+The system is peer-to-peer, and transactions take place between users directly, without an intermediary.[13]:4 These transactions are verified by network nodes and recorded in a public distributed ledger called a blockchain. Since the system works without a central repository or single administrator, bitcoin is called the first decentralized digital currency.[13]:1[16]
+
+Besides being created as a reward for mining, bitcoin can be exchanged for other currencies,[17] products, and services in legal or black markets.[18][19]
+
+As of February 2015, over 100,000 merchants and vendors accepted bitcoin as payment.[20] According to research produced by Cambridge University in 2017, there are 2.9 to 5.8 million unique users using a cryptocurrency wallet, most of them using bitcoin.
+            </p>'
+};
+
+function createTemp(data)
+{
+  var title = data.title;
+  var date = data.date;
+  var heading = data.heading;
+  var content = data.content;
+var htmltemp ='
+<html>
+    <head>
+        <title>
+            $(title);
+        </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+         <div>
+            <a href="/">Home</a>
+        </div>
+        <div>
+            Date :<b>$(date)</b>
+        </div>
+         <div >
+            <h1>$(heading)</h1>
+        </div>
+        <div>
+        $(content)
+        </div>
+        </div>
+    </body>
+</html>';
+return htmltemp;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemp(article-one));
 });
 app.get('/article-two',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
