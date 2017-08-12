4f8ebfc728b4fa2746220b4843140cc93f77df94
Akash Jindal <30688034+ajind033@users.noreply.github.com>
2017-08-12 16:17:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleone1 ={
-    title: 'Article-One|Bitcoin',
-    date: '12/August/2017',
-    heading: 'Article-One|Bitcoin',
-    content:  `<p>
-                Bitcoin is a worldwide cryptocurrency and digital payment system[13]:3 invented by an unknown programmer, or a group of programmers, under the name Satoshi Nakamoto.[14] It was released as open-source software in 2009.[15]
-                
-The system is peer-to-peer, and transactions take place between users directly, without an intermediary.[13]:4 These transactions are verified by network nodes and recorded in a public distributed ledger called a blockchain. Since the system works without a central repository or single administrator, bitcoin is called the first decentralized digital currency.[13]:1[16]
-
-Besides being created as a reward for mining, bitcoin can be exchanged for other currencies,[17] products, and services in legal or black markets.[18][19]
-
-As of February 2015, over 100,000 merchants and vendors accepted bitcoin as payment.[20] According to research produced by Cambridge University in 2017, there are 2.9 to 5.8 million unique users using a cryptocurrency wallet, most of them using bitcoin.
-            </p> `
-} ;
 var articles ={
     articleone :{
     title: 'Article-One|Bitcoin',
@@ -64,11 +50,13 @@ As of February 2015, over 100,000 merchants and vendors accepted bitcoin as paym
 },
 };
 
-function createTemp(data){  var title = data.title;
+function createTemp(data){  
+  var title = data.title;
   var date = data.date;
   var heading = data.heading;
   var content = data.content;
-var htmltemp =`
+
+    var htmltemp =`
 <html>
     <head>
         <title>
