5c9e6d91cde068c0908b7c31449f93b3a7de60a6
Akash Jindal <30688034+ajind033@users.noreply.github.com>
2017-08-13 15:47:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ button.onclick = function(){
   };
   
   //Make a request.
-  request.open("GET","http://ajind033.imad.hasura-app.io/counter",true);
+  request.open("GET","https://ajind033.imad.hasura-app.io/counter",true);
   request.send();
 };
 
