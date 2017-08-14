d202e56c102387064734641e3e19ce7b79ca2868
Akash Jindal <30688034+ajind033@users.noreply.github.com>
2017-08-14 14:43:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,31 +31,56 @@ button.onclick = function(){
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
     
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
+  // Create a request object.
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  
+  request.onreadystatechange = function(){
       
-      if(this.readystate === 4 && this.status === 200){
-          var names= this.responseText;
+      if(this.readyState === 4) {
+          //Take some task
           
-          document.getElementById("temp").innerHTML = names;
-          names = JSON.parse(names);
-          var list="";
-          
-          for(var i=0; i<names.length; i++){
-              list="<li>"+names[i]+"</li>";
+          if(this.status === 200){
+              document.getElementById("temp").innerHTML = this.responseText;
           }
-         
-          document.getElementById("temp").innerHTML = list;
-      }  
-    };
-    
-    var nameinput = document.getElementById("name");
-    var name = nameinput.value;
-    
-  request.open("GET","http://ajind033.imad.hasura-app.io/submit-name?name=" + name,true);
+      }
+      //Not done Yet.
+  };
+  var nameinput = document.getElementById("name");
+  var  name = nameinput.value;
+  //Make a request.
+  request.open("GET","http://ajind033.imad.hasura-app.io/submit-name?name="+name,true);
   request.send(null);
 };
 
+/*
+
+button.onclick = function(){
+  
+  // Create a request object.
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  
+  request.onreadystatechange = function(){
+      
+      if(this.readyState === 4) {
+          //Take some task
+          
+          if(this.status === 200){
+              document.getElementById("count").innerHTML = this.responseText;
+          }
+      }
+      //Not done Yet.
+  };
+  
+  //Make a request.
+  request.open("GET","http://ajind033.imad.hasura-app.io/counter",true);
+  request.send(null);
+};
+
+*/
 /*
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
